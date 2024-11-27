a3366484d9d1bebcd5cf3f05b0cacfa9b0f20b29
Manuel Nuila <manuel.nuila@cuemby.com>
2024-11-27 09:25:45 -0600

    fix deprecated interpolation

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ resource "helm_release" "prometheus_msteams" {
   values = [
     yamlencode({
       connectors = [{
-        "${var.alert_channel}" = var.alert_webhook
+        var.alert_channel = var.alert_webhook
       }]
       container = {
         additionalArgs = ["-debug"]
